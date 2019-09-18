f0b03d7d61defb1431a7f4317fb1847cc462881a
Akinnigbagbe Femi <oakinnigbagbe@eminenttechnology.com>
2019-09-18 11:09:28 +0100

    new commit

---
+++ JobPortal@@/Model/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobPortal2.Model
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+
+        public virtual ICollection<EmployerCompany> EmployerCompanies { get; set; }
+    }
+}
---
+++ JobPortal@@/Model/ForgotPasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobPortal2.Model
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
